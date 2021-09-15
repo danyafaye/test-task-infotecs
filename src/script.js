34446fa732cbff374459ff6891be156bebb50efd
Danila Serpokrylov <serpokrylov.danila@mail.ru>
2021-09-15 20:02:19 +0300

    added sort in descending order and content to each column in what order it in

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const aboutInfo = document.querySelector('.table_about')
 const tableTh = document.querySelectorAll('th')
 const tableBody = document.querySelector('tbody')
+const iArrow = document.querySelectorAll('i')
 
 //ответ на запрос json массива
 fetch('./src/data.json')
@@ -9,6 +10,7 @@ fetch('./src/data.json')
     })
     .then((data) => {
         fillTable(data)
+        sortTable()
     })
 
 //функция для заполнения таблицы нужными данными
@@ -26,15 +28,36 @@ let fillTable = (data) => {
     }
 }
 
+//наложение события на заголовки с сортировкой отдельной колонки
+let sortTable = () => {
+    tableTh.forEach((th,n)=>{
+        th.addEventListener('click', () => {
+            checkIfSelectedTh(n)
+            if(!th.dataset.index || th.dataset.index === '-1'){
+                th.setAttribute('data-index', 1)
+            } else if (th.dataset.index === '1'){
+                th.setAttribute('data-index', -1)
+            }
+            let data = th.dataset.index
+            th.classList.add('selected')
+            sortRows(n, data)
+        })
+    })}
+
 //функция для сортировки таблицы index - колонка, которую требуется отсортировать
-let sortTable = (index) => {
+let sortRows = (index, data) => {
     let sortedRows = Array.from(tableBody.rows)
-        .sort((rowA, rowB) => rowA.cells[index].innerHTML > rowB.cells[index].innerHTML ? 1 : -1);
+        .sort((rowA, rowB) => rowA.cells[index].innerHTML > rowB.cells[index].innerHTML ? data : -data);
 
     tableBody.append(...sortedRows);
 }
 
-//наложение события на заголовки с сортировкой отдельной колонки
-tableTh.forEach((th,i)=>{
-    th.addEventListener('click', () => (sortTable(i)))
-})
+//функция, которая проверяет класс selected у заголовков таблицы и которая убирает его у невыбранных заголовков
+let checkIfSelectedTh = (index) => {
+    tableTh.forEach((th, n)=>{
+        if(th.classList.contains('selected') && n!==index){
+            th.classList.toggle('selected');
+            th.removeAttribute('data-index')
+        }
+    })
+}
