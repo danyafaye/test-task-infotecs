ae54d47945a7fc60ed03383e49a35510046dc0a0
Danila Serpokrylov <serpokrylov.danila@mail.ru>
2021-09-18 00:06:08 +0300

    added form by clicking on a table

--- src/script.js
+++ src/script.js
@@ -1,17 +1,12 @@
 const aboutInfo = document.querySelector('.table_about')
 const tableTh = document.querySelectorAll('th')
 const tableBody = document.querySelector('tbody')
-const iArrow = document.querySelectorAll('i')
-
-//ответ на запрос json массива
-fetch('./src/data.json')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        fillTable(data)
-        sortTable()
-    })
+const table = document.querySelector('table')
+const form = document.querySelector('.form_wrapper')
+const input = document.querySelectorAll('input')
+const textArea = document.querySelector('textarea')
+const editButton = document.querySelector('.div_bttn_edit')
+const closeButton = document.querySelector('.div_bttn_close')
 
 //функция для заполнения таблицы нужными данными
 //about/4 для вывода примерно двух строк
@@ -19,6 +14,7 @@ let fillTable = (data) => {
     const aboutInfoLength = aboutInfo.clientWidth
     for (let i = 0; i < data.length; i++) {
         let myTr = document.createElement('tr')
+        myTr.className = 'table_rows'
         myTr.innerHTML = `<td class="table_first_name _col">${data[i].name.firstName}</td>
             <td class="table_last_name _col">${data[i].name.lastName}</td>
             <td class="table_about _col">${data[i].about.slice(0, (aboutInfoLength / 4)) + '...'}</td>
@@ -61,3 +57,28 @@ let checkIfSelectedTh = (index) => {
         }
     })
 }
+
+let editTable = () => {
+    table.addEventListener('click', (table)=>{
+        const row = table.target.closest('.table_rows')
+        form.style.cssText = `display: block;  top: ${row.offsetTop}px; left: ${row.offsetWidth + 20}px;`;
+        input[0].value = row.cells[0].innerHTML
+        input[1].value = row.cells[1].innerHTML
+        textArea.value = row.cells[2].innerHTML.slice(0, row.cells[2].innerHTML.length - 3);
+        input[2].value = row.cells[3].innerHTML
+    })
+    editButton.addEventListener('click', () => {
+
+    })
+}
+
+//ответ на запрос json массива
+fetch('./src/data.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        fillTable(data)
+        sortTable()
+        editTable()
+    })
