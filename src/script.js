e34cb80d5400ed17044ed3dbad1186b16e21eb10
Danila Serpokrylov <serpokrylov.danila@mail.ru>
2021-09-18 22:34:13 +0300

    added editing with form and used localstorage

--- src/script.js
+++ src/script.js
@@ -1,44 +1,41 @@
-const aboutInfo = document.querySelector('.table_about')
-const tableTh = document.querySelectorAll('th')
-const tableBody = document.querySelector('tbody')
-const table = document.querySelector('table')
-const form = document.querySelector('.form_wrapper')
-const input = document.querySelectorAll('input')
-const textArea = document.querySelector('textarea')
-const editButton = document.querySelector('.div_bttn_edit')
-const closeButton = document.querySelector('.div_bttn_close')
-
+const tableTh = document.querySelectorAll('th'),
+tableBody = document.querySelector('tbody')
 //функция для заполнения таблицы нужными данными
 //about/4 для вывода примерно двух строк
-let fillTable = (data) => {
-    const aboutInfoLength = aboutInfo.clientWidth
-    for (let i = 0; i < data.length; i++) {
+let fillTable = (jsonData) => {
+    localStorage.getItem('jsonData') ? '' : localStorage.setItem('jsonData', JSON.stringify(jsonData));
+    const aboutInfo = document.querySelector('.table_about'),
+        aboutInfoLength = aboutInfo.clientWidth,
+        data = localStorage.getItem('jsonData') ? JSON.parse(localStorage.getItem('jsonData')) : jsonData
+    tableBody.innerHTML = ''
+    data.forEach((el) => {
         let myTr = document.createElement('tr')
+        myTr.setAttribute('id', el.id)
         myTr.className = 'table_rows'
-        myTr.innerHTML = `<td class="table_first_name _col">${data[i].name.firstName}</td>
-            <td class="table_last_name _col">${data[i].name.lastName}</td>
-            <td class="table_about _col">${data[i].about.slice(0, (aboutInfoLength / 4)) + '...'}</td>
-            <td class="table_eye_color _col">${data[i].eyeColor}</td>`
-        let myTb = document.querySelector('tbody')
-        myTb.appendChild(myTr)
-    }
+        myTr.innerHTML = `<td class="table_first_name _col">${el.name.firstName}</td>
+            <td class="table_last_name _col">${el.name.lastName}</td>
+            <td class="table_about _col">${el.about.slice(0, (aboutInfoLength / 4)) + '...'}</td>
+            <td class="table_eye_color _col">${el.eyeColor}</td>`
+        tableBody.appendChild(myTr)
+    })
 }
 
 //наложение события на заголовки с сортировкой отдельной колонки
 let sortTable = () => {
-    tableTh.forEach((th,n)=>{
+    tableTh.forEach((th, n) => {
         th.addEventListener('click', () => {
             checkIfSelectedTh(n)
-            if(!th.dataset.index || th.dataset.index === '-1'){
+            if (!th.dataset.index || th.dataset.index === '-1') {
                 th.setAttribute('data-index', 1)
-            } else if (th.dataset.index === '1'){
+            } else if (th.dataset.index === '1') {
                 th.setAttribute('data-index', -1)
             }
             let data = th.dataset.index
             th.classList.add('selected')
             sortRows(n, data)
         })
-    })}
+    })
+}
 
 //функция для сортировки таблицы index - колонка, которую требуется отсортировать
 let sortRows = (index, data) => {
@@ -50,26 +47,57 @@ let sortRows = (index, data) => {
 
 //функция, которая проверяет класс selected у заголовков таблицы и которая убирает его у невыбранных заголовков
 let checkIfSelectedTh = (index) => {
-    tableTh.forEach((th, n)=>{
-        if(th.classList.contains('selected') && n!==index){
+    tableTh.forEach((th, n) => {
+        if (th.classList.contains('selected') && n !== index) {
             th.classList.toggle('selected');
             th.removeAttribute('data-index')
         }
     })
 }
 
+//функция для редактирования таблицы
 let editTable = () => {
-    table.addEventListener('click', (table)=>{
-        const row = table.target.closest('.table_rows')
+    const table = document.querySelector('table'),
+        form = document.querySelector('.form_wrapper'),
+        input = document.querySelectorAll('input'),
+        textArea = document.querySelector('textarea'),
+        editButton = document.querySelector('.div_bttn_edit'),
+        closeButton = document.querySelector('.div_bttn_close')
+    let dataRow
+    //вызов формы и передача данных в нее
+    table.addEventListener('click', (event) => {
+        const row = event.target.closest('.table_rows')
+        dataRow = row;
         form.style.cssText = `display: block;  top: ${row.offsetTop}px; left: ${row.offsetWidth + 20}px;`;
         input[0].value = row.cells[0].innerHTML
         input[1].value = row.cells[1].innerHTML
         textArea.value = row.cells[2].innerHTML.slice(0, row.cells[2].innerHTML.length - 3);
         input[2].value = row.cells[3].innerHTML
     })
+    //событие для внесения изменений в json файл при клике на кнопку редактирования
     editButton.addEventListener('click', () => {
-
+        const jsonData = JSON.parse(localStorage.getItem('jsonData'));
+        const editedRow = {
+            id: dataRow.id,
+            name: {
+                firstName: input[0].value,
+                lastName: input[1].value
+            },
+            phone: dataRow.phone,
+            about: textArea.value,
+            eyeColor: input[2].value
+        }
+        jsonData.forEach((item, n) => {
+            if (item.id === dataRow.id) {
+                jsonData.splice(n, 1, editedRow)
+            }
+        })
+        localStorage.setItem('jsonData', JSON.stringify(jsonData));
+        form.style = '';
+        fillTable(jsonData);
     })
+    //событие для закрытия окна формы
+    closeButton.addEventListener('click', () => form.style = '')
 }
 
 //ответ на запрос json массива
